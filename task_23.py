dfba7b8db723c30820de4c1c63fef5ffe5d444f9
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 21:34:26 +0300

    1

--- task_23.py
+++ task_23.py
@@ -5,7 +5,25 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_6_6():
-    pass
+    while True:
+        if wall_is_on_the_right:
+            break
+        else:
+            move_right()
+            if not wall_is_above():
+                while True:
+                    if wall_is_above():
+
+                        break
+                    else:
+                        move_up()
+                        fill_cell()
+                    while not wall_is_beneath():
+                            move_down()
+    while not wall_is_on_the_left:
+        move_left()
+
+    move_right()
 
 
 if __name__ == '__main__':
