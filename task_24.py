e9b8cdc8ae121cabea33448246918b0209c4f809
Eugene Kegeles <zhenyakeg@gmail.com>
2016-09-22 21:04:38 +0300

    1

--- task_24.py
+++ task_24.py
@@ -5,7 +5,25 @@ from pyrob.api import *
 
 @task
 def task_2_1():
-    pass
+    def star():
+        move_right()
+        fill_cell()
+        move_right()
+        move_down()
+        fill_cell()
+        move_down()
+        move_left()
+        fill_cell()
+        move_up()
+        fill_cell()
+        move_left()
+        fill_cell()
+        move_up()
+    move_right()
+    move_down()
+    star()
+
+
 
 
 if __name__ == '__main__':
