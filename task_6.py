a14307206fb7d4680f1415f755c6b6aa8845c9c0
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 19:09:40 +0300

    1

--- task_6.py
+++ task_6.py
@@ -5,7 +5,10 @@ from pyrob.api import *
 
 @task
 def task_5_3():
-    pass
+    while wall_is_beneath() == False:
+        move_right()
+    while wall_is_beneath() == True:
+        move_right()
 
 
 if __name__ == '__main__':
