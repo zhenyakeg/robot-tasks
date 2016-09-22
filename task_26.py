0dea2cd3c4363a867e0a38db3ac130ee7963588c
Eugene Kegeles <zhenyakeg@gmail.com>
2016-09-22 21:27:13 +0300

    1

--- task_26.py
+++ task_26.py
@@ -3,9 +3,34 @@
 from pyrob.api import *
 
 
-@task(delay=0.02)
+@task(delay=0.002)
 def task_2_4():
-    pass
+    def star():
+        move_right()
+        fill_cell()
+        move_right()
+        move_down()
+        fill_cell()
+        move_down()
+        move_left()
+        fill_cell()
+        move_up()
+        fill_cell()
+        move_left()
+        fill_cell()
+        move_up()
+    move_right(36)
+    for i in range (9):
+        for j in range (4):
+            star()
+            move_down(4)
+        star()
+        move_up(16)
+        move_left(4)
+    for j in range(4):
+        star()
+        move_down(4)
+    star()
 
 
 if __name__ == '__main__':
