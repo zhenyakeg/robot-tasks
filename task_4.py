bf0ededf04144ee6352caa8b106359ceb49f107a
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 19:04:40 +0300

    1

--- task_4.py
+++ task_4.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-    pass
+    if wall_is_on_the_right() == False:
+        move_right()
+    elif wall_is_on_the_left() == False:
+        move_left()
+    elif wall_is_above() == False:
+        move_up()
+    elif wall_is_beneath()== False:
+        move_down()
 
 
 if __name__ == '__main__':
