ebbfa69b6020aa29b7bfa2c71a1360388b920eeb
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 19:37:02 +0300

    1

--- task_9.py
+++ task_9.py
@@ -3,9 +3,14 @@
 from pyrob.api import *
 
 
-@task
+@task (delay=0.01)
 def task_8_2():
-    pass
+    while not wall_is_on_the_right():
+        if not wall_is_above():
+            fill_cell()
+        move_right()
+    if not wall_is_above():
+        fill_cell();
 
 
 if __name__ == '__main__':
