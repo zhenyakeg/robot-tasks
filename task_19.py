ecf55ba17eabcfbf4bfe91e5a4914cc66d8bf750
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 20:48:33 +0300

    1

--- task_19.py
+++ task_19.py
@@ -3,9 +3,29 @@
 from pyrob.api import *
 
 
-@task
+@task (delay=0.01)
 def task_8_29():
-    pass
+    a = False
+
+    while not wall_is_on_the_left():
+        move_left()
+    if not wall_is_above():
+        a = True
+    else:
+        while not wall_is_on_the_right():
+            move_right()
+        if not wall_is_above():
+            a = True
+    if a:
+        while not wall_is_above():
+            move_up()
+        while not wall_is_on_the_left():
+            move_left()
+
+
+
+
+
 
 
 if __name__ == '__main__':
