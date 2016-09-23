1881be83a9d8ff0e2ad90f6008263cf645d4c5ff
Eugene Kegeles <zhenyakeg@gmail.com>
2016-09-24 00:17:03 +0300

    1

--- task_28.py
+++ task_28.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    k=0
+    while not wall_is_on_the_right():
+        if cell_is_filled():
+            k+=1
+        if k==5:
+            return
+        else:
+            move_right()
 
 
 if __name__ == '__main__':
